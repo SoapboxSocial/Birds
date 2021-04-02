dea522563146d1cf8553c9ad9c60ff07b913c7a8
decanus <7621705+decanus@users.noreply.github.com>
2021-04-02 17:53:19 +0200

    fix

--- lib/birds/playersManager.ts
+++ lib/birds/playersManager.ts
@@ -6,11 +6,12 @@ import Scores from "./scoreSystem";
 
 const NB_AVAILABLE_BIRDS_COLOR = 4;
 
-let _playersList: Player[] = [];
-let _posOnGrid = 0;
-let _scores = new Scores();
-
 export default class PlayersManager extends EventEmitter {
+
+  private playersList: Player[] = [];
+  private posOnGrid = 0;
+  private scores = new Scores();
+
   addNewPlayer(playerSocket: any, id: string) {
     // Set an available color according the number of client's sprites
     const birdColor = Math.floor(
@@ -20,11 +21,11 @@ export default class PlayersManager extends EventEmitter {
     // Create new player and add it in the list
     const newPlayer = new Player(playerSocket, id, birdColor);
 
-    _playersList.push(newPlayer);
+    this.playersList.push(newPlayer);
 
     console.info(
       "New player connected. There is currently " +
-        _playersList.length +
+        this.playersList.length +
         " player(s)"
     );
 
@@ -32,34 +33,34 @@ export default class PlayersManager extends EventEmitter {
   }
 
   removePlayer(player: Player) {
-    const pos = _playersList.indexOf(player);
+    const pos = this.playersList.indexOf(player);
 
     if (pos < 0) {
       console.error("[ERROR] Can't find player in playerList");
     } else {
       console.info("Removing player " + player.getNick());
 
-      _playersList.splice(pos, 1);
+      this.playersList.splice(pos, 1);
 
-      console.info("It remains " + _playersList.length + " player(s)");
+      console.info("It remains " + this.playersList.length + " player(s)");
     }
   }
 
   changeLobbyState(player: Player, isReady: boolean) {
-    let pos = _playersList.indexOf(player);
+    let pos = this.playersList.indexOf(player);
 
     if (pos < 0) {
       console.error("[ERROR] Can't find player in playerList");
     } else {
       // Change ready state
-      _playersList[pos].setReadyState(isReady);
+      this.playersList[pos].setReadyState(isReady);
     }
 
     // PlayersManager check if players are ready
-    for (let i = 0; i < _playersList.length; i++) {
+    for (let i = 0; i < this.playersList.length; i++) {
       // if at least one player doesn't ready, return
-      if (_playersList[i].getState() === enums.PlayerState.WaitingInLobby) {
-        const nick = _playersList[i].getNick();
+      if (this.playersList[i].getState() === enums.PlayerState.WaitingInLobby) {
+        const nick = this.playersList[i].getNick();
 
         console.info(nick + " is not yet ready, don't start game");
 
@@ -74,13 +75,13 @@ export default class PlayersManager extends EventEmitter {
   getPlayerList(specificState?: PlayerStateEnum) {
     let players = [];
 
-    for (let i = 0; i < _playersList.length; i++) {
+    for (let i = 0; i < this.playersList.length; i++) {
       if (specificState) {
-        if (_playersList[i].getState() === specificState) {
-          players.push(_playersList[i]);
+        if (this.playersList[i].getState() === specificState) {
+          players.push(this.playersList[i]);
         }
       } else {
-        players.push(_playersList[i].getPlayerObject());
+        players.push(this.playersList[i].getPlayerObject());
       }
     }
 
@@ -90,30 +91,30 @@ export default class PlayersManager extends EventEmitter {
   getOnGamePlayerList() {
     let players = [];
 
-    for (let i = 0; i < _playersList.length; i++) {
+    for (let i = 0; i < this.playersList.length; i++) {
       if (
-        _playersList[i].getState() === enums.PlayerState.Playing ||
-        _playersList[i].getState() === enums.PlayerState.Died
+        this.playersList[i].getState() === enums.PlayerState.Playing ||
+        this.playersList[i].getState() === enums.PlayerState.Died
       )
-        players.push(_playersList[i].getPlayerObject());
+        players.push(this.playersList[i].getPlayerObject());
     }
 
     return players;
   }
 
   getNumberOfPlayers() {
-    return _playersList.length;
+    return this.playersList.length;
   }
 
   updatePlayers(time: number) {
-    for (let i = 0; i < _playersList.length; i++) {
-      _playersList[i].update(time);
+    for (let i = 0; i < this.playersList.length; i++) {
+      this.playersList[i].update(time);
     }
   }
 
   arePlayersStillAlive() {
-    for (let i = 0; i < _playersList.length; i++) {
-      if (_playersList[i].getState() === enums.PlayerState.Playing) {
+    for (let i = 0; i < this.playersList.length; i++) {
+      if (this.playersList[i].getState() === enums.PlayerState.Playing) {
         return true;
       }
     }
@@ -125,12 +126,12 @@ export default class PlayersManager extends EventEmitter {
     let updatedList = [];
 
     // reset position counter
-    _posOnGrid = 0;
+    this.posOnGrid = 0;
 
-    for (let i = 0; i < _playersList.length; i++) {
-      _playersList[i].preparePlayer(_posOnGrid++);
+    for (let i = 0; i < this.playersList.length; i++) {
+      this.playersList[i].preparePlayer(this.posOnGrid++);
 
-      updatedList.push(_playersList[i].getPlayerObject());
+      updatedList.push(this.playersList[i].getPlayerObject());
     }
 
     return updatedList;
@@ -138,15 +139,15 @@ export default class PlayersManager extends EventEmitter {
 
   sendPlayerScore() {
     // Save player score
-    for (let i = 0; i < _playersList.length; i++) {
-      _scores.savePlayerScore(_playersList[i], _playersList[i].getScore());
+    for (let i = 0; i < this.playersList.length; i++) {
+      this.scores.savePlayerScore(this.playersList[i], this.playersList[i].getScore());
     }
 
     // Retrieve highscores and then send scores to players
-    _scores.getHighScores(function (highScores) {
+    this.scores.getHighScores((highScores) => {
       // Send score to the players
-      for (let i = 0; i < _playersList.length; i++) {
-        _playersList[i].sendScore(_playersList.length, highScores);
+      for (let i = 0; i < this.playersList.length; i++) {
+        this.playersList[i].sendScore(this.playersList.length, highScores);
       }
     });
   }
@@ -158,9 +159,9 @@ export default class PlayersManager extends EventEmitter {
     player.setFloor(floor);
 
     // Retrieve his highscore
-    _scores.setPlayerHighScore(player);
+    this.scores.setPlayerHighScore(player);
 
     // Put him on the game grid
-    player.preparePlayer(_posOnGrid++);
+    player.preparePlayer(this.posOnGrid++);
   }
 }
