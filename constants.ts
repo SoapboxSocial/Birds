38582942773dfab3cbf150e668c1d80172cf0c2c
Jeff Reiner <theundeadghost@gmail.com>
2021-04-02 19:47:29 +0200

    Fix path

--- constants.ts
+++ constants.ts
@@ -1,8 +1,7 @@
 export const constants = {
   SERVER_PORT: 4242,
   SOCKET_PORT: 1337,
-  SOCKET_ADDR:
-    "https://soapbox-apps-server-e782y.ondigitalocean.app/birds/birds",
+  SOCKET_ADDR: "https://soapbox-apps-server-e782y.ondigitalocean.app/birds",
 
   SCREEN_WIDTH: 900,
   SCREEN_HEIGHT: 768,
--- sharedConstants.js
+++ sharedConstants.js
@@ -2,8 +2,7 @@
 const constant = {
   SERVER_PORT: 4242,
   SOCKET_PORT: 1337,
-  SOCKET_ADDR:
-    "https://soapbox-apps-server-e782y.ondigitalocean.app/birds/birds",
+  SOCKET_ADDR: "https://soapbox-apps-server-e782y.ondigitalocean.app/birds",
 
   SCREEN_WIDTH: 900,
   SCREEN_HEIGHT: 768,
