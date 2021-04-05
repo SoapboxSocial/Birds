7a36ad02a3cd9c1d01f003e61c0f427f1b96d72c
Jeff Reiner <theundeadghost@gmail.com>
2021-04-05 11:59:50 +0200

    Moving to a monorepo

---
+++ api/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+// import { constants as Const } from "./constants";
+import { start } from "./lib/birds";
+
+// const app = express();
+
+/**
+ * Setup
+ */
+// app.set("PORT", Const.SERVER_PORT);
+
+// app.listen(app.get("PORT"), () => {
+//   console.log(`🐦 [server]: listening at ${app.get("PORT")}`);
+// });
+
+start();
