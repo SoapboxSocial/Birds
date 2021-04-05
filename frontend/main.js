ecc8b2c56b8328eea379f27e4bba42493cd58501
Jeff Reiner <theundeadghost@gmail.com>
2021-04-05 12:48:23 +0200

    Change resource path on client

--- frontend/main.js
+++ frontend/main.js
@@ -136,7 +136,7 @@ async function startClient(nick) {
     "Connecting to the server...";
 
   _socket = io.connect("/api", {
-    resource: "api",
+    resource: "api/socket.io",
     query: `roomID=${roomID}`,
     reconnect: false,
   });
